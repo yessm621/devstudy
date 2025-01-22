fd788a18405928ad96d2bbf44ad2399b52a579fd
yessm621 <yessm621@gmail.com>
2025-01-22 17:40:02 +0900

    first commit

---
+++ src/main/java/me/devstudy/DevstudyApplication.java
@@ -0,0 +1,13 @@
+package me.devstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/devstudy/DevstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.devstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
