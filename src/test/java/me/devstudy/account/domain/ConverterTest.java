08856c0de6f664eb6699fa315f7a45dff30cafc1
yessm621 <yessm621@gmail.com>
2025-01-27 01:20:48 +0900

    converter 테스트 코드 작성

---
+++ src/test/java/me/devstudy/account/domain/ConverterTest.java
@@ -0,0 +1,23 @@
+package me.devstudy.account.domain;
+
+import me.devstudy.account.domain.support.ListStringConverter;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConverterTest {
+
+    @Test
+    void ListStringConverterTest() {
+        ListStringConverter converter = new ListStringConverter();
+        List<String> inputList = List.of("hello", "java", "world");
+
+        String convertedToDb = converter.convertToDatabaseColumn(inputList);
+        List<String> convertedToEntity = converter.convertToEntityAttribute(convertedToDb);
+
+        assertThat(convertedToDb).isEqualTo("hello,java,world");
+        assertThat(convertedToEntity).containsExactly("hello", "java", "world");
+    }
+}
